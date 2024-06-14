7ac859ea8671e610d0bfcdc22b0f48c38ebbff70
Philipp Claßen <philipp.classen@posteo.de>
2024-06-14 05:37:26 +0200

    Add support for non-string types

---
+++ spec/env-config_spec.cr
@@ -0,0 +1,297 @@
+require "./spec_helper"
+
+old_foo = ENV["FOO"]?
+ENV["FOO"] = nil
+
+class ConfigDefaultVar < EnvConfig
+  expect_env FOO, description: "Test variable", default: "foo"
+end
+
+ENV["FOO"] = old_foo
+
+describe ConfigDefaultVar do
+  it "works" do
+    ConfigDefaultVar::FOO.should eq("foo")
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+old_bar = ENV["BAR"]?
+ENV["FOO"] = "foo_from_env"
+ENV["BAR"] = "bar_from_env"
+
+class ConfigFromEnv < EnvConfig
+  expect_env FOO, description: "Test variable", default: "foo"
+  expect_env BAR, description: "Test variable"
+end
+
+ENV["FOO"] = old_foo
+ENV["BAR"] = old_bar
+
+describe ConfigFromEnv do
+  it "works" do
+    ConfigFromEnv::FOO.should eq("foo_from_env")
+    ConfigFromEnv::BAR.should eq("bar_from_env")
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+ENV["FOO"] = nil
+
+class ConfigMissing < EnvConfig
+  @@missing_key = ""
+
+  def self.terminate_handler!(key) : Nil
+    @@missing_key = key
+  end
+
+  def self.missing_key
+    @@missing_key
+  end
+
+  expect_env FOO, description: "This will fail, since it is not set"
+end
+
+ENV["FOO"] = old_foo
+
+describe ConfigMissing do
+  it "works" do
+    ConfigMissing.missing_key.should eq("FOO")
+    ConfigMissing::FOO # that it not optimized away
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+old_bar = ENV["BAR"]?
+ENV["FOO"] = "foo_from_env"
+ENV["BAR"] = "bar_from_env"
+
+class ConfigWithBlock < EnvConfig
+  config do
+    expect_env FOO, description: "Test variable"
+    expect_env BAR, description: "Test variable"
+  end
+end
+
+ENV["FOO"] = old_foo
+ENV["BAR"] = old_bar
+
+describe ConfigWithBlock do
+  it "works" do
+    ConfigWithBlock::FOO.should eq("foo_from_env")
+    ConfigWithBlock::BAR.should eq("bar_from_env")
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+old_bar = ENV["BAR"]?
+ENV["FOO"] = "foo_from_env"
+ENV["BAR"] = "bar_from_env"
+
+class ConfigWithNamedBlock < EnvConfig
+  config("database") do
+    expect_env FOO, description: "Test variable"
+  end
+
+  config("logging") do
+    expect_env BAR, description: "Test variable"
+  end
+end
+
+ENV["FOO"] = old_foo
+ENV["BAR"] = old_bar
+
+describe ConfigWithNamedBlock do
+  it "works" do
+    ConfigWithNamedBlock::FOO.should eq("foo_from_env")
+    ConfigWithNamedBlock::BAR.should eq("bar_from_env")
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+old_bar = ENV["BAR"]?
+ENV["FOO"] = "42"
+ENV["BAR"] = "2024-06-14"
+
+class ConfigWithRegexp < EnvConfig
+  config do
+    expect_env FOO, description: "Integer", type: Int32, regexp: NUMBER
+    expect_env BAR, description: "YYYY-MM-DD", regexp: /^\d{4}-\d{2}-\d{2}$/
+  end
+end
+
+ENV["FOO"] = old_foo
+ENV["BAR"] = old_bar
+
+describe ConfigWithRegexp do
+  it "works" do
+    ConfigWithRegexp::FOO.should eq(42)
+    ConfigWithRegexp::BAR.should eq("2024-06-14")
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+ENV["FOO"] = nil
+
+class ConfigWithFlagDefault < EnvConfig
+  config do
+    expect_env FOO, description: "Bool", default: true
+  end
+end
+
+ENV["FOO"] = old_foo
+
+describe ConfigWithFlagDefault do
+  it "works" do
+    ConfigWithFlagDefault::FOO.should eq(true)
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+ENV["FOO"] = "true"
+
+class ConfigWithFlagEnv < EnvConfig
+  config do
+    expect_env FOO, description: "Bool", type: Bool
+  end
+end
+
+ENV["FOO"] = old_foo
+
+describe ConfigWithFlagEnv do
+  it "works" do
+    ConfigWithFlagEnv::FOO.should eq(true)
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+ENV["FOO"] = nil
+
+class ConfigWithInt64Default < EnvConfig
+  config do
+    expect_env FOO, description: "Int64 to be detected by default", default: 4000000000_i64
+  end
+end
+
+ENV["FOO"] = old_foo
+
+describe ConfigWithInt64Default do
+  it "works" do
+    ConfigWithInt64Default::FOO.should eq(4000000000_i64)
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+ENV["FOO"] = "5000000000"
+
+class ConfigWithInt64Env < EnvConfig
+  config do
+    expect_env FOO, description: "Int64 to be detected by type", type: Int64
+  end
+end
+
+ENV["FOO"] = old_foo
+
+describe ConfigWithInt64Env do
+  it "works" do
+    ConfigWithInt64Env::FOO.should eq(5000000000_i64)
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+ENV["FOO"] = "3.14"
+
+class ConfigWithFloat32Env < EnvConfig
+  config do
+    expect_env FOO, description: "Float32 to be detected by type", type: Float32
+  end
+end
+
+ENV["FOO"] = old_foo
+
+describe ConfigWithFloat32Env do
+  it "works" do
+    ConfigWithFloat32Env::FOO.should eq(3.14_f32)
+    ConfigWithFloat32Env::FOO.class.should eq(Float32)
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+ENV["FOO"] = "3.14"
+
+class ConfigWithFloat64Env < EnvConfig
+  config do
+    expect_env FOO, description: "Float64 to be detected by type", type: Float64
+  end
+end
+
+ENV["FOO"] = old_foo
+
+describe ConfigWithFloat64Env do
+  it "works" do
+    ConfigWithFloat64Env::FOO.should eq(3.14_f64)
+    ConfigWithFloat64Env::FOO.class.should eq(Float64)
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+ENV["FOO"] = nil
+
+class ConfigWithFloat32Default < EnvConfig
+  config do
+    expect_env FOO, description: "Float32 to be detected by default", default: 3.14_f32
+  end
+end
+
+ENV["FOO"] = old_foo
+
+describe ConfigWithFloat32Env do
+  it "works" do
+    ConfigWithFloat32Env::FOO.should eq(3.14_f32)
+    ConfigWithFloat32Env::FOO.class.should eq(Float32)
+  end
+end
+
+######################################################################
+
+old_foo = ENV["FOO"]?
+ENV["FOO"] = nil
+
+class ConfigWithFloat64Default < EnvConfig
+  config do
+    expect_env FOO, description: "Float64 to be detected by default", default: 3.14_f64
+  end
+end
+
+ENV["FOO"] = old_foo
+
+describe ConfigWithFloat64Env do
+  it "works" do
+    ConfigWithFloat64Env::FOO.should eq(3.14_f64)
+    ConfigWithFloat64Env::FOO.class.should eq(Float64)
+  end
+end
--- src/env-config.cr
+++ src/env-config.cr
@@ -3,6 +3,17 @@ require "log"
 abstract class EnvConfig
   Log = ::Log.for("env-config")
 
+  def self.to_bool(flag : String) : Bool
+    case flag.strip.downcase
+    when "true", "1", "on", "enabled"
+      true
+    when "false", "0", "off", "disabled"
+      false
+    else
+      raise "Failed to parse boolean <<#{flag}>> (try \"true\" or \"false\" instead)"
+    end
+  end
+
   # ameba:disable Metrics/CyclomaticComplexity
   def self.read_env_variable(key, **options)
     value = ENV.fetch(key, options[:default]?.to_s || "")
@@ -14,14 +25,28 @@ abstract class EnvConfig
       terminate_handler!(key)
     end
 
-    supported_types = {Int32, String}
+    supported_types = {String, Bool, Int32, Int64, Float32, Float64}
     if options[:type]? && !supported_types.includes?(options[:type]?)
       raise "Got unsupported type=#{options[:type]?} (must one of #{supported_types})"
     end
 
-    options[:regexp]?.try do |format|
+    target =
+      if options[:type]?
+        options[:type]?
+      elsif options[:default]?
+        options[:default]?.class
+      else
+        String
+      end
+
+    regexp = options[:regexp]?
+    if !regexp && target == Bool
+      regexp = FLAG
+    end
+
+    regexp.try do |format|
       unless format.match(value)
-        puts "ERROR: environment variable #{key} is ill-formed (got: <<#{value}>>, but expected: #{format.source})"
+        puts "ERROR: environment variable #{key} is ill-formed (got: <<#{value}>>, but expected: #{pretty_regexp(format)})"
         puts "Description: #{options[:description]}" if options[:description]
         puts "Example: #{options[:example]?}" if options[:example]?
         puts "Default: #{options[:default]?}" if options[:default]?
@@ -35,15 +60,49 @@ abstract class EnvConfig
       Log.info { "#{key} => #{value}" }
     end
 
-    if options[:type]? == Int32
-      value.to_i
-    else
+    if target == String
       value
+    elsif target == Bool
+      to_bool(value)
+    elsif target == Int32
+      value.to_i
+    elsif target == Int64
+      value.to_i64
+    elsif target == Float32
+      value.to_f32
+    elsif target == Float64
+      value.to_f64
+    else
+      puts "ERROR: unsupported type detected for environment variable #{key} (got: <<#{value}>>, deduced <<#{value}}>> of type <<#{target}>>)"
+      puts "Description: #{options[:description]}" if options[:description]
+      puts "Example: #{options[:example]?}" if options[:example]?
+      puts "Default: #{options[:default]?}" if options[:default]?
+      puts "Hint: if nothing works, try to use a String, and then write your own converter."
+      terminate_handler!(key)
     end
   end
 
   macro expect_env(name, **options)
-    {{name}} = read_env_variable "{{name}}", {{options.double_splat}}
+    {% if options[:type] %}
+      {{name}} = (read_env_variable "{{name}}", {{options.double_splat}}).as({{ options[:type] }})
+    {% elsif options[:default].is_a?(NumberLiteral) %}
+      {% if options[:default].kind == :i32 %}
+        {{name}} = (read_env_variable "{{name}}", {{options.double_splat}}).as(Int32)
+      {% elsif options[:default].kind == :i64 %}
+        {{name}} = (read_env_variable "{{name}}", {{options.double_splat}}).as(Int64)
+      {% elsif options[:default].kind == :f32 %}
+        {{name}} = (read_env_variable "{{name}}", {{options.double_splat}}).as(Float32)
+      {% elsif options[:default].kind == :f64 %}
+        {{name}} = (read_env_variable "{{name}}", {{options.double_splat}}).as(Float64)
+      {% else %}
+        # TODO: can this be reached?
+        throw "Unexpected NumberLiteralType"
+      {% end %}
+    {% elsif options[:default].is_a?(BoolLiteral) %}
+      {{name}} = (read_env_variable "{{name}}", {{options.double_splat}}).as(Bool)
+    {% else %}
+      {{name}} = (read_env_variable "{{name}}", {{options.double_splat}}).to_s
+    {% end %}
   end
 
   macro config(conf = "config")
@@ -54,6 +113,18 @@ abstract class EnvConfig
 
   # predefined matchers:
   NUMBER = /^[0-9]+$/
+  FLAG   = /\A(?:true|1|false|0)\z/i
+
+  def self.pretty_regexp(regexp : Regex) : String
+    case regexp
+    when NUMBER
+      %q("#{regexp.source} (a non-negative integer)")
+    when FLAG
+      %q("#{regexp.source} (a boolean flag: "true","1","on","enabled" vs "false","0","off","disabled" ==> false"))
+    else
+      regexp.source
+    end
+  end
 
   # If you do not want to exit, you can override the function. But note that
   # the error is by design unrecoverable. You can throw an exception instead,
